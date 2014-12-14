4aa4bdd908195866ac78798dfabdd7f9c877f56a
Hassan Hayat <hassan.hayat7@gmail.com>
2014-12-14 18:05:57 +0000

    Added documentation to Scene

--- Engine/Scene/Scene.elm
+++ Engine/Scene/Scene.elm
@@ -1,5 +1,22 @@
 module Engine.Scene.Scene where
 
+{-| This module defines the Scene type and the default Scene object.
+A Scene contains a camera (the viewer's window to the scene), a list of
+Renderable objects (the stuff that gets drawn to the scene), a light
+(something to illuminate the scene), and a Viewport (a description of
+the context the scene gets drawn to).
+
+Note: Currently, Graphics Engine only supports having one light. In future
+releases, this will most certainly change in order to support having multiple
+lights in a scene.
+
+Note: Currently, Graphics Engine onlu supports having one camera. While this
+may be all you need in most cases, it may be valuable to allow for multiple
+cameras. Currently, the only way to have multiple cameras is to have
+multiple scenes. It is still an open question on which approach is better.
+
+-}
+
 import Engine.Camera.Camera (Camera, camera)
 import Engine.Render.Renderable (Renderable)
 import Engine.Light.Light (Light, light)
@@ -13,6 +30,14 @@ import Engine.Viewport.Viewport (Viewport, viewport)
 
 -- TODO: Find a strategy to deal with multiple materials
 
+{-| Represents a scene. A scene contains a list of objects such that
+calling `render` on a scene will render all the objects in a webgl context.
+
+A scene contains a camera to define the viewer's viewing point, a light to
+illuminate the scene, and a viewport to describe the context on which the
+scene will be drawn.
+
+-}
 type alias Scene = {
   camera    : Camera,
   objects   : List Renderable,
@@ -20,6 +45,9 @@ type alias Scene = {
   viewport  : Viewport
 }
 
+{-| Default scene object. Draws a red cube in the middle of the default context.
+
+-}
 scene : Scene
 scene = {
   camera   = camera,
