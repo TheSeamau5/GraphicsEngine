68e7f1774cc01af0d1843cd89ddb73c67a457b76
Hassan Hayat <hassan.hayat7@gmail.com>
2014-12-14 17:55:39 +0000

    Added documentation to Renderable

--- Engine/Render/DefaultRenderable.elm
+++ Engine/Render/DefaultRenderable.elm
@@ -1,7 +1,20 @@
 module Engine.Render.DefaultRenderable where
 
+{-| This module contains the definition for the default Renderable renderable.
+This definition is separated from the Renderable module in order to avoid
+circular dependencies.
+
+
+# Default Renderable
+@docs renderable
+
+-}
+
 import Engine.Render.Renderable (Renderable)
 import Engine.Mesh.Cube (cube)
 
+{-| Default renderable object. Alias for the default cube object.
+
+-}
 renderable : Renderable
 renderable = cube
--- Engine/Render/Renderable.elm
+++ Engine/Render/Renderable.elm
@@ -1,10 +1,30 @@
 module Engine.Render.Renderable where
 
+{-| This module contains the definition of the Renderable type. This is
+separated from the definition of the default renderable object in order to
+avoid circular dependencies.
+
+# Definition
+@docs Renderable
+
+-}
+
 import Engine.Transform.Transform (Transform)
 import Engine.Material.Material (Material)
 import Engine.Mesh.Mesh (Mesh)
 import Engine.Object.Object (Object)
 
+{-| Represent a renderable object. A renderable object is an Object with a
+Transform properties (position, rotation, and scale) in order to locate it
+physically in world space. A renderable object also comes with a Material
+and a Mesh.
+
+A renderable uses its position, rotation, and scale to set its position,
+rotation and scale in world space respectively. The material describes how
+the renderable object responds to light and draws itself. The mesh describes
+the geometry of the renderable object (if it is a cube, a pyramid, etc...).
+
+-}
 type alias Renderable = Object (Transform {
   material : Material,
   mesh     : Mesh
