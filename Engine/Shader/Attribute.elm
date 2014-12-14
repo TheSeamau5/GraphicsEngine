bdfd2850471e26c4b9f6c182fc765ca2d34b6a26
Hassan Hayat <hassan.hayat7@gmail.com>
2014-12-14 18:15:23 +0000

    Added documentation to Attribute

--- Engine/Shader/Attribute.elm
+++ Engine/Shader/Attribute.elm
@@ -1,5 +1,21 @@
 module Engine.Shader.Attribute where
 
+{-| This module contains the definition of the Attribute type.
+A vertex shader takes a set of attributes as input for each vertex.
+The Attibute type is made to capture all of these attributes.
+
+Currently, we only pass the position to the vertex shader as an attibute.
+
+Typically, a color and a normal is also passed as an attribute but for
+simplicity it is highly encouraged to set the color at the material level
+and to retrieve the normal from the normal matrix (which is automatically
+passed as a uniform).
+
+# Definition
+@docs Attribute
+
+-}
+
 import Math.Vector3 (Vec3)
 
 -- TODO: Reconsider the properties of the Attribute type
