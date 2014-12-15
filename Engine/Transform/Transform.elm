abccc8417dd6f7f7b08eae74ebf3de6437fed403
Hassan Hayat <hassan.hayat7@gmail.com>
2014-12-15 15:57:09 +0000

    Added documentation to Transform

--- Engine/Transform/Transform.elm
+++ Engine/Transform/Transform.elm
@@ -1,14 +1,41 @@
 module Engine.Transform.Transform where
 
+{-| This module contains the definition for the Transform type and
+the default transform object.
+
+# Definition
+@docs Transform
+
+# Default Transform
+@docs transform
+
+-}
+
 import Math.Vector3 (Vec3, vec3)
 
+{-| A transform is an object with a position, a rotation, and a scale.
+This is mean to be able to represent linear transformations in space. As
+such, one can model where an object is (position), what is its orientation
+(rotation), and how large is it (scale).
 
+-}
 type alias Transform a = { a |
   position : Vec3,
   rotation : Vec3,
   scale    : Vec3
 }
 
+
+{-| Default transform.
+
+Defined as follows:
+
+    transform = {
+      position = vec3 0 0 0,
+      rotation = vec3 0 0 0,
+      scale    = vec3 1 1 1 }
+
+-}
 transform : Transform {}
 transform = {
   position = vec3 0 0 0,
