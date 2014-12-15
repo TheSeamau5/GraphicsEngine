71051bfeaae46fa3eb0b99e77c3e9a5ccbc960d6
Hassan Hayat <hassan.hayat7@gmail.com>
2014-12-15 16:02:18 +0000

    Added documentation to Viewport

--- Engine/Viewport/Viewport.elm
+++ Engine/Viewport/Viewport.elm
@@ -1,5 +1,24 @@
 module Engine.Viewport.Viewport where
 
+{-| This module contains the definition for the Viewport type and the
+default viewport object
+
+# Definition
+@docs Viewport
+
+# Default Viewport
+@docs viewport
+
+-}
+
+
+
+{-| A Viewport is an object that is supposed to describe the target "screen"
+or "context" on to which a scene is rendered.
+
+A Viewport has dimensions consisting of a width and a height.
+
+-}
 type alias Viewport = {
   dimensions : {
       width  : Float,
@@ -7,6 +26,13 @@ type alias Viewport = {
   }
 }
 
+
+
+{-| Default Viewport. Sets dimensions.width = 400 and dimensions.height = 400.
+(In essence, with the default viewport object your scene will appear in
+a 400 by 400 canvas or webgl context).
+
+-}
 viewport : Viewport
 viewport = {
   dimensions = {
